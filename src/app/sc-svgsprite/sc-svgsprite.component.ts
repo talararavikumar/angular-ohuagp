10dd3d521d43e96cdc3d9818189c858b1e7989ea
talararavikumar <ravikumar.nitw@gmail.com>
2019-09-25 19:26:31 +0530

    3 files added

---
+++ src/app/sc-svgsprite/sc-svgsprite.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sc-svgsprite',
+  templateUrl: './sc-svgsprite.component.html'
+})
+export class ScSvgspriteComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
