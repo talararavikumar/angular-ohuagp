41480676bd72d4c0633f854bd961c0cd2442878c
talararavikumar <ravikumar.nitw@gmail.com>
2019-09-25 17:30:55 +0530

    before after sample

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,13 @@ import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { AngularSampleListComponent } from './angular-sample-list/angular-sample-list.component'
 import {DynamicComponentComponent} from './dynamic-component/dynamic-component.component'
+import {Beforaftersample1Component} from './beforaftersample1/beforaftersample1.component'
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent, AngularSampleListComponent ,DynamicComponentComponent],
-  bootstrap:    [ AppComponent ]
+  declarations: [ AppComponent, HelloComponent, AngularSampleListComponent ,DynamicComponentComponent,
+  Beforaftersample1Component],
+  bootstrap:    [ AppComponent ],
+  entryComponents:[]
 })
 export class AppModule { }
---
+++ src/app/beforaftersample1/beforaftersample1.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-beforaftersample1',
+  templateUrl: './beforaftersample1.component.html',
+  styleUrls: ['./beforaftersample1.component.scss']
+})
+export class Beforaftersample1Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
