6d4cf1c8eb6e4c5473381b2bd36f316aa5de4d98
talararavikumar <ravikumar.nitw@gmail.com>
2019-09-25 19:17:53 +0530

    6 files added

---
+++ src/app/flatarrow/flatarrow.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-flatarrow',
+  templateUrl: './flatarrow.component.html',
+  styleUrls: ['./flatarrow.component.css']
+})
+export class FlatarrowComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/flexsample/flexsample.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-flexsample',
+  templateUrl: './flexsample.component.html',
+  styleUrls: ['./flexsample.component.css']
+})
+export class FlexsampleComponent implements OnInit {
+
+  public sIconId:string = "mdbgImage";
+  constructor() { }
+ 
+  ngOnInit() {
+  }
+
+}
